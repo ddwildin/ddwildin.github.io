24d3c5b3bf71bb81f47646312cbf364effc966ae
Daniel <ddwildin@gmail.com>
2021-12-27 21:46:30 +0900

    Improve main index

---
+++ resources/js/script.js
@@ -0,0 +1,14 @@
+// #battleships p, #excursion p, #teacozy p, #fotomatic p, #companyhomepage p, #responsiveclub p
+console.log('Im loaded!');
+const names = ['battleships', 'excursion', 'teacozy', 'fotomatic', 'companyhomepage', 'responsiveclub'];
+const createEvents = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        document.getElementById(arr[i]).firstElementChild.onmouseover = (event) => {
+            document.getElementById(arr[i]).lastElementChild.style.display = 'block';
+        };
+        document.getElementById(arr[i]).firstElementChild.onmouseout = (event) => {
+            document.getElementById(arr[i]).lastElementChild.style.display = 'none';
+        };
+    }
+};
+createEvents(names);
