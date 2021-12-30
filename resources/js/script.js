766201e629034d47a60b88362033799874ba4af8
Daniel <ddwildin@gmail.com>
2021-12-30 19:54:04 +0900

    Update main index adding weather app to list

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,6 @@
 // #battleships p, #excursion p, #teacozy p, #fotomatic p, #companyhomepage p, #responsiveclub p
 console.log('Im loaded!');
-const names = ['battleships', 'excursion', 'teacozy', 'fotomatic', 'companyhomepage', 'responsiveclub'];
+const names = ['battleships', 'excursion', 'teacozy', 'fotomatic', 'companyhomepage', 'responsiveclub', 'weatherapp'];
 const createEvents = (arr) => {
     for (let i = 0; i < arr.length; i++) {
         document.getElementById(arr[i]).firstElementChild.onmouseover = (event) => {
